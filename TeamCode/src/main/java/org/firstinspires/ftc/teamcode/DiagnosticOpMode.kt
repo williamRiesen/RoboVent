f7e257b2c6e653b10f2f3c3e8df8bfbd95ca8c10
williamRiesen <williamriesen@gmail.com>
2020-05-04 19:01:27 -0500

    Everything works; about to attempt isPhoneChargeEnabled

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagnosticOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagnosticOpMode.kt
@@ -1,31 +1,17 @@
 package org.firstinspires.ftc.teamcode
 
-import android.util.Log
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import com.qualcomm.robotcore.util.ElapsedTime
 
 @TeleOp(name = "Diagnostic OpMode", group = "Iterative Opmode")
 class DiagnosticOpMode: OpMode() {
-    private val runtime = ElapsedTime()
-    private var loopCount = 0
-
 
     override fun init() {
         telemetry.addData("Status", "Initialized")
     }
 
     override fun loop() {
-        if ( runtime.seconds() > 60) resetStartTime()
-        loopCount += 1
-        telemetry.addData("Loop Count", loopCount)
-        telemetry.addData("Status", "Run Time: $runtime")
+        Thread.sleep(1000)
     }
-
-    override fun stop() {
-        super.stop()
-        Log.v("Yip", "Yip!")
-    }
-
 
 }
